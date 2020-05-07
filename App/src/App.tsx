4276071ca55dd42205bef7d0ec56e097a288af11
Łukasz Gandecki <4002543+lgandecki@users.noreply.github.com>
2020-05-07 11:38:33 +0200

    final version

--- App/src/App.tsx
+++ App/src/App.tsx
@@ -1,7 +1,17 @@
 import * as React from "react";
+import { Title } from "xolvio_ui/components/Title";
+import { Sections } from "xolvio_ui/components/Sections";
+import { CenteredContentWrapper } from "xolvio_ui/helpers/CenteredContentWrapper";
+import { Background } from "xolvio_ui/elements/Background";
+import { content } from "./content";
 
 function App() {
-  return <div>This is my React App!</div>;
+  return (
+    <CenteredContentWrapper>
+      <Background />
+      <Title subheading={content.subheading} title={content.title} />
+      <Sections sections={content.sections} />
+    </CenteredContentWrapper>
+  );
 }
-
 export default App;
