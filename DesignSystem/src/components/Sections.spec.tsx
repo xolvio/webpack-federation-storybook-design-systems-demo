dd25178bb570604775f469fc290bab5d6a1ab52e
Łukasz Gandecki <4002543+lgandecki@users.noreply.github.com>
2020-05-26 18:59:56 +0200

    fixed types in the spec

--- DesignSystem/src/components/Sections.spec.tsx
+++ DesignSystem/src/components/Sections.spec.tsx
@@ -3,14 +3,14 @@ import {Section} from "xolvio_ui/components/Sections";
 import {render} from "@testing-library/react"
 
 test('Should render a Heading', () => {
-  const {getByText} = render(<Section description={""} heading={"My Heading"}/>)
+  const {getByText} = render(<Section description={""} heading={"My Heading"} icon={""}/>)
 
   getByText("My Heading")
 })
 
 
 test('Should render a Description', () => {
-  const {getByText} = render(<Section description={"My Description"} heading={""}/>)
+  const {getByText} = render(<Section description={"My Description"} heading={""} icon={""}/>)
 
   getByText("My Description")
 })
