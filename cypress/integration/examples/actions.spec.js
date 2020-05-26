f233b70dc6c7bc48b21e003f3b8dd83f39e2065f
Łukasz Gandecki <4002543+lgandecki@users.noreply.github.com>
2020-05-26 16:05:32 +0200

    cypress testing on CI

---
+++ cypress/integration/examples/actions.spec.js
@@ -0,0 +1,18 @@
+const getAutomaticallyDeployedURL = () => {
+  const branchName = Cypress.env("GITHUB_REF").replace("refs/heads/", "")
+  return `https://xolvio-ui.s3.amazonaws.com/${branchName}/App/index.html`
+}
+
+context('Smoke test', () => {
+  beforeEach(() => {
+    const path = getAutomaticallyDeployedURL()
+    console.log("GOZDECKI path", path)
+    cy.visit(getAutomaticallyDeployedURL())
+  })
+
+  it('The app shows up', () => {
+    // https://on.cypress.io/type
+    cy.findByText(/Prepare your team and product for success./)
+  })
+
+})
